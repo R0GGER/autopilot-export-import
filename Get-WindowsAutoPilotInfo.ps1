7c5bc8a3016ddad644b877d724dd06d3674f757d
R0GGER <8298741+R0GGER@users.noreply.github.com>
2024-05-27 11:13:57 +0200

    Create Get-WindowsAutoPilotInfo.ps1

---
+++ Get-WindowsAutoPilotInfo.ps1
@@ -0,0 +1,25 @@
+$Serialnumber = (Get-CimInstance -Class Win32_BIOS).SerialNumber
+$date = Get-Date -Format yyyyMMdd
+$Path = ($MyInvocation.MyCommand.Path -replace $MyInvocation.MyCommand.Name, "") + "AutoPilot-Export\"  
+if (!(Test-Path $Path)) {
+    New-Item -ItemType Directory -Path $Path
+}
+$FileName = $date + "_Autopilot_Export.csv"
+$FullPath = $Path + $FileName
+
+# HardwareHash
+$HardwareHash = (Get-CimInstance -Namespace root/CIMV2/mdm/dmmap -ClassName MDM_DevDetail_Ext01).DeviceHardwareData
+
+# Create CSV
+$output = [PSCustomObject]@{
+    "Device Serial Number" = $Serialnumber;
+    "Windows Product ID"   = "";
+    "Hardware Hash"        = $HardwareHash
+} 
+
+if (!(Test-Path -Path $FullPath)) {
+    $output | Export-Csv -LiteralPath $FullPath -Delimiter "," -Encoding utf8 -Force -NoTypeInformation
+}
+else {
+    $output | Export-Csv -LiteralPath $FullPath -Delimiter "," -Encoding utf8 -Force -Append
+}
