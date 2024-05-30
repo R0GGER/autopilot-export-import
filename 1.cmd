c0017cf64fe96718dfe9f68adadd75ffa0ef0f84
R0GGER <8298741+R0GGER@users.noreply.github.com>
2024-05-30 12:33:46 +0200

    Update 1.cmd

--- 1.cmd
+++ 1.cmd
@@ -1,1 +1,1 @@
-Powershell Set-ExecutionPolicy Unrestricted
+PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command %~d0\Get-WindowsAutoPilotInfo.ps1 -OutputFile %~d0\AutoPilotHWID.csv -append
